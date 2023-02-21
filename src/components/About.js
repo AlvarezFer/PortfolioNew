d4d35b621f5ef95ada9180db4029d41edc91fa77
AlvarezFer <fernicomeres@gmail.com>
2023-02-20 23:28:19 -0300

    Se agrego funcionalidad

--- src/components/About.js
+++ src/components/About.js
@@ -8,12 +8,12 @@ const About = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-24">
           <img
-            className="object-cover h-full w-[420px] md:mx-auto lg:mx-0 rounded-2xl"
+            className="object-cover h-full w-[400px] md:mx-auto lg:mx-0 rounded-2xl"
             src={Image}
             alt=""
           />
           <div className="flex flex-col items-center text-center lg:items-start lg:text-left">
-            <div className="flex flex-col" style={{ marginTop: "160px" }}>
+            <div className="flex flex-col" style={{ marginTop: "80px" }}>
               <h2
                 className="text-3xl lg:text-4xl font-medium lg:font-extrabold mb-3 before:content-about relative before:absolute before:opacity-40 before:-top-[2rem] before:hidden before:lg:block"
                 style={{ fontFamily: "Ubuntu, sans-serif" }}
